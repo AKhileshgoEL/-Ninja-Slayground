2cd80fd3753e19ff69d25f0eeb9c53f210a46e61
AKhileshgoEL <97514693+AKhileshgoEL@users.noreply.github.com>
2023-08-18 20:41:06 +0530

    Update Solution.cpp

--- Day 1/Solution.cpp
+++ Day 1/Solution.cpp
@@ -1,1 +1,14 @@
+int dataTypes(string type) {
+	if(type[0]=='I'){
+		return 4 ;
+	}else if(type[0]=='L'){
+		return 8;
+	}else if(type[0]=='F'){
+		return 4;
+	}else if(type[0]=='D'){
+		return 8;
+	}else{
+		return 1;
+	}
+}
 
